26497f0a9c9e7144c7e2a8d24fe7d0dcb8431ffd
Carter Hale <44034905+HaleCT2@users.noreply.github.com>
2018-11-15 18:27:05 -0500

    Added Game Layout
    Includes Player Movement, Car Drawings, and General Graphical Layout.

--- ECE287_Project/hw_image_generator.vhd
+++ ECE287_Project/hw_image_generator.vhd
@@ -25,9 +25,17 @@ USE ieee.std_logic_1164.all;
 
 ENTITY hw_image_generator IS
 	GENERIC(
-		pixels_Y :	INTEGER := 50;    --row that first color will persist until
-		pixels_X	:	INTEGER := 50);   --column that first color will persist until
+		lane1_s : INTEGER := 608;
+		lane1_e : INTEGER := 656;
+		lane2_s : INTEGER := 1264;
+		lane2_e : INTEGER := 1312;
+		carwidth_s : INTEGER := 202;
+		carwidth_e : INTEGER := 405;
+		carlength_s : INTEGER := 760;
+		carlength_e : INTEGER := 1030);   --column that first color will persist until
 	PORT(
+		button_left :  IN		STD_LOGIC;
+		button_right : IN		STD_LOGIC;
 		disp_ena		:	IN		STD_LOGIC;	--display enable ('1' = display time, '0' = blanking time)
 		row			:	IN		INTEGER;		--row pixel coordinate
 		column		:	IN		INTEGER;		--column pixel coordinate
@@ -37,18 +45,43 @@ ENTITY hw_image_generator IS
 END hw_image_generator;
 
 ARCHITECTURE behavior OF hw_image_generator IS
+SIGNAL carstate : STD_LOGIC_VECTOR(0 to 1);
 BEGIN
-	PROCESS(disp_ena, row, column)
+	PROCESS(disp_ena, row, column, button_left, button_right)
 	BEGIN
 
 		IF(disp_ena = '1') THEN		--display time
-			IF(row < pixels_Y AND column < pixels_X) THEN
-				red <= (OTHERS => '0');
-				green	<= (OTHERS => '0');
-				blue <= (OTHERS => '1');
-			ELSE
+			IF (row > lane1_s AND row < lane1_e) THEN
 				red <= (OTHERS => '1');
 				green	<= (OTHERS => '1');
+				blue <= (OTHERS => '1');
+			ELSIF (row > lane2_s AND row < lane2_e) THEN
+				red <= (OTHERS => '1');
+				green	<= (OTHERS => '1');
+				blue <= (OTHERS => '1');
+			ELSIF (button_left = '0' AND button_right = '1' AND row > carwidth_s AND row < carwidth_e AND column > carlength_s AND column < carlength_e) THEN
+				red <= ("00110011");
+				green	<= ("10011001");
+				blue <= ("00110011");
+				carstate <= "00";
+			ELSIF (button_left = '1' AND button_right = '1' AND row > (carwidth_s + 658) AND row < (carwidth_e + 658) AND column > carlength_s AND column < carlength_e) THEN
+				red <= ("00110011");
+				green	<= ("10011001");
+				blue <= ("00110011");
+				carstate <= "01";
+			ELSIF (button_left = '0' AND button_right = '0' AND row > (carwidth_s + 658) AND row < (carwidth_e + 658) AND column > carlength_s AND column < carlength_e) THEN
+				red <= ("00110011");
+				green	<= ("10011001");
+				blue <= ("00110011");
+				carstate <= "01";
+			ELSIF (button_left = '1' AND button_right = '0' AND row > (carwidth_s + 1316) AND row < (carwidth_e + 1316) AND column > carlength_s AND column < carlength_e) THEN
+				red <= ("00110011");
+				green	<= ("10011001");
+				blue <= ("00110011");
+				carstate <= "10";
+			ELSE
+				red <= (OTHERS => '0');
+				green	<= (OTHERS => '0');
 				blue <= (OTHERS => '0');
 			END IF;
 		ELSE								--blanking time
